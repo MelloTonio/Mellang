406962ef874a892dc15f6a9468139b1fb4407e37
MelloTonio <antoniomelloaviao@gmail.com>
2021-01-03 15:31:13 -0300

    feat: new test case

--- Lexer/lexer_test.go
+++ Lexer/lexer_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	/*input := `=+(){},;`
 
 	tests := []struct {
 		expectedType    Token.TokenType
@@ -23,6 +23,64 @@ func TestNextToken(t *testing.T) {
 		{Token.SEMICOLON, ";"},
 		{Token.EOF, ""},
 	}
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}*/
+
+	input := `let five = 5;let ten = 10;let add = fn(x, y) {x + y;};let result = add(five, ten);`
+
+	tests := []struct {
+		expectedType    Token.TokenType
+		expectedLiteral string
+	}{
+		{Token.MOONVAR, "moonvar"},
+		{Token.IDENT, "five"},
+		{Token.ASSIGN, "="},
+		{Token.INT, "5"},
+		{Token.SEMICOLON, ";"},
+		{Token.MOONVAR, "moonvar"},
+		{Token.IDENT, "ten"},
+		{Token.ASSIGN, "="},
+		{Token.INT, "10"},
+		{Token.SEMICOLON, ";"},
+		{Token.MOONVAR, "moonvar"},
+		{Token.IDENT, "add"},
+		{Token.ASSIGN, "="},
+		{Token.FUNCTION, "fn"},
+		{Token.LPAREN, "("},
+		{Token.IDENT, "x"},
+		{Token.COMMA, ","},
+		{Token.IDENT, "y"},
+		{Token.RPAREN, ")"},
+		{Token.LBRACE, "{"},
+		{Token.IDENT, "x"},
+		{Token.PLUS, "+"},
+		{Token.IDENT, "y"},
+		{Token.SEMICOLON, ";"},
+		{Token.RBRACE, "}"},
+		{Token.SEMICOLON, ";"},
+		{Token.MOONVAR, "moonvar"},
+		{Token.IDENT, "result"},
+		{Token.ASSIGN, "="},
+		{Token.IDENT, "add"},
+		{Token.LPAREN, "("},
+		{Token.IDENT, "five"},
+		{Token.COMMA, ","},
+		{Token.IDENT, "ten"},
+		{Token.RPAREN, ")"},
+		{Token.SEMICOLON, ";"},
+		{Token.EOF, ""},
+	}
+
 	l := New(input)
 
 	for i, tt := range tests {
