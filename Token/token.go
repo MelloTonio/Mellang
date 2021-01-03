4eb30f5c4a7e0738c1206bdf49a5cb1ea65d0d31
MelloTonio <antoniomelloaviao@gmail.com>
2021-01-03 14:38:46 -0300

    feat: more tokens

--- Token/token.go
+++ Token/token.go
@@ -2,6 +2,47 @@ package Token
 
 type TokenType string
 
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identificadores + Literais
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 123456789 ...
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimitadores
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	MOONVAR  = "MOONVAR"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
 type Token struct {
 	Type    TokenType
 	Literal string
