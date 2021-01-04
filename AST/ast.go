78f65c32aba94de7e025fea1a7d2252a19ce786e
antonio mello babo <antoniomelloaviao@gmail.com>
2021-01-04 15:41:17 -0300

    feat: Conditions

--- AST/ast.go
+++ AST/ast.go
@@ -133,6 +133,25 @@ func (pe *PrefixExpression) String() string {
 	return out.String()
 }
 
+func (ie *IfExpression) expressionNode()      {}
+func (ie *IfExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+
+	if ie.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(ie.Alternative.String())
+	}
+
+	return out.String()
+}
+
 // AST TREE
 /*
 STATEMENTS -> MoonvarStatement (name, value)
